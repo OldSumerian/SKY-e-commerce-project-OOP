96004fd0ce3c2d85e80e6cab4c9fec4b7831f42e
Sumerian <shemerov.s@yandex.ru>
2024-03-25 15:20:05 +0300

    add utils.py

---
+++ src/utils.py
@@ -0,0 +1,11 @@
+import json
+
+
+def get_info_from_json():
+    """
+    Получает данные из json файла
+    :return:
+    """
+    with open('data/products.json', encoding='UTF-8') as data:
+        full_products_info = json.load(data)
+    return full_products_info
