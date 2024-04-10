04428297f6bbf978f6fea508b070669bac7aa31a
Sumerian <shemerov.s@yandex.ru>
2024-04-10 22:33:57 +0300

    add abstract class

--- main.py
+++ main.py
@@ -47,6 +47,8 @@ def main():
     # print()
     # print(products[0][0] + products[0][1])
 
+
+    # Создание экземпляра класса-наследника
     any_smart = classes.class_Cat_Smartphone.Smartphone('ExampleSmart', 'Powerful smart',
                                                         100000, 5, 'black', 3000,
                                                         'S22 Ultra', '512 Gb')
@@ -54,17 +56,20 @@ def main():
     print(repr(any_smart))
     print()
 
+    # Создание экземпляра другого класса-наследника
     any_grass = classes.class_Cat_Grass.Grass('SomeGrass', 'Very good grass', 5000,
                                               6, 'green', 'Russia', 2)
     print(any_grass)
     print(repr(any_grass))
     print()
 
+    # Создание экземпляра класса-родителя
     any_product = classes.class_Product.Product('MyTV', 'Must Have!', 70000, 1, 'white')
     print(any_product)
     print(repr(any_product))
     print()
 
+    # Создание еще одного экземпляра класса-наследника (для проверки методов)
     second_smart = classes.class_Cat_Smartphone.Smartphone('AnotherSmart', 'New Powerful smart',
                                                         150000, 20, 'black', 3000,
                                                         'S24 Ultra', '512 Gb')
@@ -72,15 +77,19 @@ def main():
     print(repr(second_smart))
     print()
 
-    print(any_smart + second_smart)
-    # print(any_smart + any_product) # Вызовется ошибка при складывании разных классов
-
+    print(f'Реализация сложения однотипных продуктов: {any_smart + second_smart}')
+    # print(any_smart + any_product) # Вызовется ошибка при складывании разных классов-наследников
+    print()
     smart_category = classes.class_Category.Category('Smartphones', 'Many good smarts', [any_smart])
     print(smart_category)
     print(smart_category.products)
     print(repr(smart_category))
-    smart_category.products(second_smart) # ВОТ ТУТ НЕ РАБОТАЕТ СЕТТЕР
-    print(smart_category)
+    # smart_category.products(second_smart) # ВОТ ТУТ НЕ РАБОТАЕТ СЕТТЕР
+    # print(smart_category)
+    print()
+    # Реализация метода предусмотренного в абстрактном базовом классе
+    print(any_smart.about_product())
+    print(any_smart.__dict__.values())
 
 if __name__ == '__main__':
     main()
